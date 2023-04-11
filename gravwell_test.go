d0f2635857f3ab4c134e1513e7d4184b869d168c
kris-watts-gravwell <kris.watts@gravwell.io>
2023-04-11 13:44:26 -0600

    adding write timeout flag to config and fixing tests

--- gravwell_test.go
+++ gravwell_test.go
@@ -10,6 +10,7 @@ package gravwellcoredns
 
 import (
 	"testing"
+	"time"
 
 	"github.com/coredns/caddy"
 )
@@ -113,6 +114,22 @@ const (
 	ingest-cache-path /tmp/dns.cache
 	max-cache-size-mb -1
 	}`
+
+	badWriteTimeoutConfig = `gravwell {
+	Ingest-Secret testing
+	Cleartext-Target 192.168.1.1:4024
+	Tag dns
+	Log-Level ERROR
+	Write-Timeout foobar
+	}`
+
+	goodWriteTimeoutConfig = `gravwell {
+	Ingest-Secret testing
+	Cleartext-Target 192.168.1.1:4024
+	Tag dns
+	Write-Timeout 900ms
+	Log-Level ERROR
+	}`
 )
 
 func TestPlay(t *testing.T) {
@@ -138,46 +155,48 @@ func TestSetupGravwell(t *testing.T) {
 
 	//test empty config
 	c := caddy.NewTestController("dns", "gravwell")
-	if _, _, _, err := parseConfig(c); err == nil {
+	if _, _, err := parseConfig(c); err == nil {
 		t.Fatal("Failed to catch empty config")
 	}
 
 	//test good config
 	c = caddy.NewTestController("dns", goodConfig)
-	if _, tag, _, err := parseConfig(c); err != nil {
+	if conf, _, err := parseConfig(c); err != nil {
 		t.Fatal(err)
-	} else if tag != `dns` {
+	} else if conf.Tag != `dns` {
 		t.Fatal("invalid tag for parse")
 	}
 
 	c = caddy.NewTestController("dns", goodConfig2)
-	if _, tag, _, err := parseConfig(c); err != nil {
+	if conf, _, err := parseConfig(c); err != nil {
 		t.Fatal(err)
-	} else if tag != `dns` {
+	} else if conf.Tag != `dns` {
 		t.Fatal("invalid tag for parse")
 	}
 
 	//check missing target
 	c = caddy.NewTestController("dns", badTargetConfig)
-	if _, _, _, err := parseConfig(c); err == nil {
+	if _, _, err := parseConfig(c); err == nil {
 		t.Fatal("Failed to catch missing targets")
 	}
 
 	//check missing tag
 	c = caddy.NewTestController("dns", badTagConfig)
-	if _, tag, _, err := parseConfig(c); err == nil || tag != `` {
-		t.Fatal("Failed to catch bad tag")
+	if conf, _, err := parseConfig(c); err != nil {
+		t.Fatal(err)
+	} else if conf.Tag != defaultTag {
+		t.Fatal("Failed to catch bad tag", conf.Tag)
 	}
 
 	//check bad log level
 	c = caddy.NewTestController("dns", badLogLevelConfig)
-	if _, _, _, err := parseConfig(c); err == nil {
+	if _, _, err := parseConfig(c); err == nil {
 		t.Fatal("Failed to catch bad log level")
 	}
 
 	//check missing encoding
 	c = caddy.NewTestController("dns", missingEncoderConfig)
-	if _, _, enc, err := parseConfig(c); err != nil {
+	if _, enc, err := parseConfig(c); err != nil {
 		t.Fatal(err)
 	} else if enc == nil {
 		t.Fatal("got a bad default encoder")
@@ -185,41 +204,54 @@ func TestSetupGravwell(t *testing.T) {
 
 	//check missing secret
 	c = caddy.NewTestController("dns", missingSecretConfig)
-	if _, _, _, err := parseConfig(c); err == nil {
+	if _, _, err := parseConfig(c); err == nil {
 		t.Fatal(err)
 	}
 
 	//check bad targets
 	c = caddy.NewTestController("dns", badTargetConfig)
-	if _, _, _, err := parseConfig(c); err == nil {
+	if _, _, err := parseConfig(c); err == nil {
 		t.Fatal("Missed bad target")
 	}
 	c = caddy.NewTestController("dns", badTarget2Config)
-	if _, _, _, err := parseConfig(c); err == nil {
+	if _, _, err := parseConfig(c); err == nil {
 		t.Fatal("Missed bad target")
 	}
 	c = caddy.NewTestController("dns", badTarget3Config)
-	if _, _, _, err := parseConfig(c); err == nil {
+	if _, _, err := parseConfig(c); err == nil {
 		t.Fatal("Missed bad target")
 	}
 
 	//check good cache
 	c = caddy.NewTestController("dns", goodCacheConfig)
-	if _, _, _, err := parseConfig(c); err != nil {
+	if _, _, err := parseConfig(c); err != nil {
 		t.Fatal(err)
 	}
 	c = caddy.NewTestController("dns", goodCache2Config)
-	if _, _, _, err := parseConfig(c); err != nil {
+	if _, _, err := parseConfig(c); err != nil {
 		t.Fatal(err)
 	}
 
 	//check bad cache
 	c = caddy.NewTestController("dns", badCacheConfig)
-	if _, _, _, err := parseConfig(c); err == nil {
+	if _, _, err := parseConfig(c); err == nil {
 		t.Fatal("Missed bad cache config")
 	}
 	c = caddy.NewTestController("dns", badCache2Config)
-	if _, _, _, err := parseConfig(c); err == nil {
+	if _, _, err := parseConfig(c); err == nil {
 		t.Fatal("Missed bad cache config")
 	}
+
+	//check timeouts
+	c = caddy.NewTestController("dns", badWriteTimeoutConfig)
+	if _, _, err := parseConfig(c); err == nil {
+		t.Fatal("Missed bad write timeout")
+	}
+	c = caddy.NewTestController("dns", goodWriteTimeoutConfig)
+	if conf, _, err := parseConfig(c); err != nil {
+		t.Fatal(err)
+	} else if conf.WriteTimeout != 900*time.Millisecond {
+		t.Fatalf("Missed write timeout %v != %v", conf.WriteTimeout, 900*time.Millisecond)
+	}
+
 }
